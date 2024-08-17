eda484165d2d06e5c5bf2bb1efd82ec1030ba819
dino_sargsyan <gevorgsargsyan22@gmail.com>
2024-08-17 22:43:21 +0400

    added auth for admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,14 +1,47 @@
+"use client";
+
 import dynamic from "next/dynamic";
-import React from "react";
+import React, { useState } from "react";
 
 // Dynamically import the Example component with no SSR
 const Example = dynamic(() => import("../../components/Admin/Example"), { ssr: false });
-const password = "kovikkovik";
 
 const AdminPage: React.FC = () => {
+  const [inputPassword, setInputPassword] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const correctPassword = "kovikkovik";
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputPassword(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (inputPassword === correctPassword) {
+      setIsAuthenticated(true);
+    } else {
+      alert("Incorrect password. Please try again.");
+    }
+  };
+
   return (
-  <Example placeholder="Enter content here..." />
-  )
+    <section className="pb-[80px] pt-[120px]">
+    <div>
+      {!isAuthenticated ? (
+        <div>
+          <input
+            type="password"
+            placeholder="Enter password"
+            value={inputPassword}
+            onChange={handlePasswordChange}
+          />
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
+      ) : (
+        <Example placeholder="Enter content here..." />
+      )}
+    </div>
+    </section>
+  );
 };
 
 export default AdminPage;
