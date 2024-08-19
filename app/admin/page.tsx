1e0123bce1e1bb487f76cc0124ecf1eda98a78ca
dino_sargsyan <gevorgsargsyan22@gmail.com>
2024-08-19 18:10:46 +0400

    Added login page for admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,6 +2,7 @@
 
 import dynamic from "next/dynamic";
 import React, { useState } from "react";
+// import {Input} from "@nextui-org/react";
 
 // Dynamically import the Example component with no SSR
 const Example = dynamic(() => import("../../components/Admin/Example"), { ssr: false });
@@ -25,16 +26,26 @@ const AdminPage: React.FC = () => {
 
   return (
     <section className="pb-[80px] pt-[120px]">
-    <div>
+    <div className="flex items-center justify-center h-screen p-10">
       {!isAuthenticated ? (
-        <div>
+        <div className="bg-gray-100 p-6 rounded-lg shadow-lg ">
+          <p>Login to Admin Page</p>
           <input
+            className="mt-4 bg-gray-50 border w-40 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             type="password"
             placeholder="Enter password"
             value={inputPassword}
             onChange={handlePasswordChange}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                handleSubmit();
+              }
+            }}
           />
-          <button onClick={handleSubmit}>Submit</button>
+          <button 
+          className="mt-4 text-white bg-blue-700  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          onClick={handleSubmit}
+          >Submit</button>
         </div>
       ) : (
         <Example placeholder="Enter content here..." />
