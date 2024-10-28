31e0a0130c0b3297b209fb8378abeb3978d68378
dino_sargsyan <gevorgsargsyan22@gmail.com>
2024-10-28 18:24:05 +0400

    deleted dictionary

--- app/[locale]/dictionaries.js
+++
@@ -1,8 +0,0 @@
-import 'server-only'
- 
-const dictionaries = {
-  en: () => import('./en.json').then((module) => module.default),
-  zh: () => import('./es.json').then((module) => module.default),
-}
- 
-export  default const getDictionary = async (locale) => dictionaries[locale]();
